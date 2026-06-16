494b5d101021fae9a1ae159b2bd5e47a8fc41813
sammansaha-sudo <samman.saha@keplor.ai>
2026-06-16 13:33:05 +0530

    Add Terraform configuration for AWS and random providers

---
+++ Terraform_basics/terraform_foundation/terraform-manifests/s1-versions.tf
@@ -0,0 +1,19 @@
+# Terraform Block
+terraform {
+ required_version = ">= 1.15.6" 
+ required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = " ~> 6.50.0"
+    }
+     random = {
+      source  = "hashicorp/random"
+      version = "~> 3.9.0"
+    }
+  }
+}
+
+# provider Block
+provider "aws" {
+  
+}
